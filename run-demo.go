c322bb3979487d45cabea6ba3923d65cd0a9864a
reus <reusee@gmail.com>
2021-05-29 00:21:05 +0800

    rename demo-server.go to run-demo.go; compile demo.wasm in run-demo.go

--- run-demo.go
+++ run-demo.go
@@ -6,9 +6,22 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
 func main() {
+
+	cmd := exec.Command("go", "build", "-o", "demo.wasm", "demo.go")
+	cmd.Env = append(os.Environ(),
+		"GOOS=js",
+		"GOARCH=wasm",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
 	dirFS := os.DirFS(".")
 	http.Handle("/", http.FileServer(http.FS(dirFS)))
 	addr := "127.0.0.1:46789"
